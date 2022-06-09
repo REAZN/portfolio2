95b50f687265068418491aba93515ebb76fd8df9
reazn <github@reazn.me>
2022-06-09 17:01:22 +0100

    projects page

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    siteUrl: "http://localhost:3000",
+    social: {
+        email: "lewis@reazn.me",
+        twitter: "@realreazn",
+        github: "REAZN",
+        linkedin: "lewisthompson-",
+        codepen: "REAZN",
+    },
+    experiments: [
+        { title: "Gradient", url: "https://codepen.io/REAZN/pen/ZEKMgBV", image: "gradient.gif" },
+        { title: "Shadow Animation", url: "https://codepen.io/REAZN/pen/KKXoKaP", image: "shadowanimation.gif" },
+        { title: "Signature", url: "https://codepen.io/REAZN/pen/PoJQbXa", image: "signature.gif" },
+        { title: "Mouse Scroll", url: "https://codepen.io/REAZN/pen/qBbRxQP", image: "mousescroll.gif" },
+        { title: "Waves", url: "https://codepen.io/REAZN/pen/eYWdEbL", image: "waves.gif" },
+        { title: "Blend Mode", url: "https://codepen.io/REAZN/pen/XWeZOxW", image: "blendmode.gif" },
+        { title: "Slinky Animation", url: "https://codepen.io/REAZN/pen/RwLMwqq", image: "slinkyanimation.gif" },
+        { title: "Slinky Animation", url: "https://codepen.io/REAZN/pen/RwLMwqq", image: "slinkyanimation.gif" },
+    ]
+}
--- pages/projects.js
+++ pages/projects.js
@@ -1,16 +1,16 @@
-import styles from "styles/pages/projects.module.scss";
+import styles from "styles/pages/Projects.module.scss";
 import { motion } from "framer-motion";
 import { Nav } from "components";
-import { useState } from "react";
+import { useState, useRef, createRef, useEffect } from "react";
+import { Link } from "react-scroll"
 
-export default function About() {
-
+export default function Projects() {
 
     const [projects, setProject] = useState([
         {
             name: "PHONE",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id lacus placerat turpis et mauris sit. Blandit risus, consequat, in odio sem eu tincidunt. Id lorem consequat arcu, ullamcorper odio. Laoreet enim, et, malesuada eget praesent. ",
-            color: "#A21C2C",
+            color: "#b10026",
             link: "something.com",
             active: false
         },
@@ -30,6 +30,15 @@ export default function About() {
         }
     ]);
 
+    const ref = useRef(Array(projects.length).fill(createRef()));
+
+    const scroll = (index) => {
+        // ref.current[index]?.current.scrollIntoView({ behavior: "smooth", block: "start" });
+        // console.log(ref.current[index], index)
+    }
+
+    useEffect(() => scroll, [])
+
     return (
         <>
             <Nav />
@@ -37,28 +46,41 @@ export default function About() {
                 <motion.div className={styles.content}>
                     <motion.div className={styles.cards}>
                         {projects.map((project, index) => (
-                            <motion.div
-                                key={project.name}
-                                className={`${styles.card} ${project.active ? styles.active : ""}`}
-                                transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
-                                onClick={() => {
-                                    project.active = !project.active;
-                                    setProject([...projects])
-                                }}
-                            >
+                            <Link to={index} name={index} smooth={true} offset={-200}
+                                key={project.name}>
                                 <motion.div
-                                    layoutId={project.name}
-                                    className={`${project.active ? styles.backgroundActive : styles.background}`}
-                                    style={{ background: project.color, zIndex: -1 + -index }}
-                                    // animation={{}}
-                                    // animate={project.active ? { originX: 0, width: "100vw" } : { originX: 1, width: "100%" }}
-                                    animate={project.active ? { borderRadius: ["0px", "50px", "100px", "50px", "0px"] } : { borderRadius: ["100px", "75px", "50px", "25px", "0px"] }}
-                                    transition={{ type: "linear", duration: 0.3 }}
-                                />
-                                <span className={styles.title}>{project.name}</span>
-                                <p>{project.description}</p>
-                                <span className={styles.link}>{project.link}</span>
-                            </motion.div>
+                                    key={project.name}
+                                    ref={ref.current[index]}
+                                    className={`${styles.card} ${project.active ? styles.active : ""}`}
+                                    // transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+                                    onClick={() => {
+                                        if (!project.active) { scroll(index); }
+                                        // console.log(ref)
+                                        project.active = !project.active;
+                                        setProject([...projects])
+                                    }}
+                                >
+                                    <motion.div
+                                        layoutId={project.name}
+                                        className={`${project.active ? styles.backgroundActive : styles.background}`}
+                                        style={{ background: project.color, zIndex: -1 + -index }}
+                                        // animation={{}}
+                                        // animate={project.active ? { originX: 0, width: "100vw" } : { originX: 1, width: "100%" }}
+                                        animate={project.active ? { borderRadius: ["0px", "50px", "100px", "50px", "0px"] } : { borderRadius: ["100px", "75px", "50px", "25px", "0px"] }}
+                                        transition={{ type: "linear", duration: 0.3 }}
+                                    />
+                                    <motion.div
+                                        // layoutId={project.name}
+                                        animate={project.active ? { marginTop: "-10%" } : ""}
+                                        className={`${styles.details} ${project.active ? styles.detailsActive : ""}`}
+                                        transition={{ type: "linear", duration: 0.3 }}
+                                    >
+                                        <span className={styles.title}>{project.name}</span>
+                                        <p className={styles.description}>{project.description}</p>
+                                        <span className={styles.link}>{project.link}</span>
+                                    </motion.div>
+                                </motion.div>
+                            </Link>
                         ))}
                     </motion.div>
                 </motion.div>
