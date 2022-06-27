063747ec0bafc6d04d2f6eaa688ac9792b75467a
reazn <github@reazn.me>
2022-06-27 03:16:18 +0100

    added new icons to exports

--- components/index.js
+++ components/index.js
@@ -1,2 +1,10 @@
 export { default as Nav } from "./Nav";
+export { default as Meta } from "./Meta";
+
+
+//Icons
 export { default as Signature } from "./icons/Signature";
+export { default as GitHub } from "./icons/social/GitHub";
+export { default as Twitter } from "./icons/social/Twitter";
+export { default as CodePen } from "./icons/social/CodePen";
+export { default as LinkedIn } from "./icons/social/LinkedIn";
